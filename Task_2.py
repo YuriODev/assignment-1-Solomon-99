dd4710e40e2f1135aebb9a8af1825c18e5843e22
Solomon-99 <161601265+Solomon-99@users.noreply.github.com>
2024-03-27 13:15:05 +0000

    First Attempt

--- Task_2.py
+++ Task_2.py
@@ -1,1 +1,23 @@
-# Your solution comes here
+number = int(input())
+numerals = ""
+
+if 1 <= number < 10 and (number != 4 and number != 9 and number != 10):
+    numerals = "V"
+    numerals = numerals + ("I" * (number - 5))
+    
+    print(numerals)
+
+elif number == 10:
+    numerals = "X"
+
+    print(numerals)
+
+elif number == 4:
+    numerals = "IV"
+
+    print(numerals)
+
+elif number == 9:
+    numerals = "IX"
+
+    print(numerals)
--- Task_5.py
+++ Task_5.py
@@ -1,1 +1,16 @@
-# Your solution comes here
+num_of_cars = int(input())
+while num_of_cars > 30 or num_of_cars < 1:
+    num_of_cars = int(input())
+
+speeds = []
+
+for i in range(num_of_cars):
+    speed = int(input())
+    speeds.append(speed)
+
+faster_than_60 = "Yes" if max(speeds) > 60 else "No"
+
+average_speed = sum(speeds) / num_of_cars
+
+print(f"{average_speed:.1f}")
+print(faster_than_60)
